04375e6c4c410caef4b74927a103f68f1dbba228
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-07-14 23:30:59 -0300

    fix: corrigindo rotas

--- index.js
+++ index.js
@@ -17,34 +17,16 @@ const cookieParser = require('cookie-parser');
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  process.env.CLUBE_CINEMA_CLIENT_URL,
-  "http://localhost:5173"
-];
+app.use(cors({origin: [process.env.CLUBE_CINEMA_CLIENT_URL, "http://localhost:5173"], credentials: true}));
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // permite curl, postman etc.
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 204
-}));
-
-app.options('*', cors());
-
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/code", codeRoutes);
-app.use("/api/v1/establishment", establishmentRoutes);
-app.use("/api/v1/client", clientRoutes);
-app.use("/api/v1/member", memberRoutes);
-app.use("/api/v1/voucher", voucherRoutes);
-app.use("/api/v1/voucherReservationHistory", voucherReservationHistory);
-app.use("/api/v1/webhook", webhook);
+app.use("/v1/user", userRoutes);
+app.use("/v1/code", codeRoutes);
+app.use("/v1/establishment", establishmentRoutes);
+app.use("/v1/client", clientRoutes);
+app.use("/v1/member", memberRoutes);
+app.use("/v1/voucher", voucherRoutes);
+app.use("/v1/voucherReservationHistory", voucherReservationHistory);
+app.use("/v1/webhook", webhook);
 
 app.listen(process.env.CLUBE_CINEMA_SERVER_PORT || 8080, () => {
   console.log("Servidor iniciado.")
